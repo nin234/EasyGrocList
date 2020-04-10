b7d409f06ae92458a7210a696e8c84cf033501aa
ninanthomas <nin234@yahoo.com>
2020-04-10 19:46:25 -0400

    bug fix

--- EasyGrocListTests/EasyGrocListTests.h
+++ EasyGrocListTests/EasyGrocListTests.h
@@ -11,3 +11,17 @@
 @interface EasyGrocListTests : XCTestCase
 
 @end
+
+//Testcase 1)
+//Alexa add milk
+//Alexa add yogurt
+//Alexa add sugar
+//Will create a new list with these 3 items
+
+//Testcase 2)
+//Alexa add milk
+//Alexa add yogurt
+//Alexa add sugar
+//Alexa add lemonade
+//Alexa remove sugar
+//Will create a new list with  3 items milk yogurt lemonade
